fcc43d1572dfdc7e0b031623a804473df3212c31
bdh36 <bdh3620@naver.com>
2021-04-10 15:15:45 +0900

    Google_Login_bdh

--- app/src/main/java/com/example/swp_dongnae/MainActivity.java
+++ app/src/main/java/com/example/swp_dongnae/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.swp_dongnae;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -8,8 +10,23 @@ import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.GoogleAuthProvider;
 import com.kakao.sdk.auth.model.OAuthToken;
 import com.kakao.sdk.user.UserApiClient;
 import com.kakao.sdk.user.model.User;
@@ -18,7 +35,11 @@ import kotlin.Unit;
 import kotlin.jvm.functions.Function1;
 import kotlin.jvm.functions.Function2;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener {
+    private SignInButton btn_google; //구글 로그인 버튼
+    private FirebaseAuth auth;  //파이어베이스 인증
+    private GoogleApiClient googleApiClient; // 구글 클라이언트 API
+    private static final int REQ_SIGN_GOOGLE = 100;// 구글 로그인 결과 코드
 
     private static final String TAG = "MainActivity";
     private View loginButton, logoutButton;
@@ -27,9 +48,50 @@ public class MainActivity extends AppCompatActivity {
 
 
     @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == REQ_SIGN_GOOGLE) {
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+                    if(result.isSuccess()){
+                        GoogleSignInAccount account = result.getSignInAccount();
+                        resultLogin(account);  //어카운트는 로그인 정보를 담고있음
+                    }
+        }
+    }
+
+    private void resultLogin(GoogleSignInAccount account) {
+        AuthCredential credential = GoogleAuthProvider.getCredential(account.getIdToken(),null);
+        auth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            Toast.makeText(MainActivity.this,"로그인 성공",Toast.LENGTH_SHORT).show();
+
+                            Intent intent = new Intent(getApplicationContext(),ResultActivity.class);
+                            intent.putExtra("nickName",account.getDisplayName());
+                            intent.putExtra("photoUrl",String.valueOf(account.getPhotoUrl())); //특정 자료형을 스트링 형태로 변환
+
+
+
+                            startActivity(intent);
+                            btn_google.setVisibility(View.GONE);
+                            logoutButton.setVisibility(View.VISIBLE);
+                        }else{
+                            Toast.makeText(MainActivity.this,"로그인 실패",Toast.LENGTH_SHORT).show();
+                            btn_google.setVisibility(View.VISIBLE);
+                            logoutButton.setVisibility(View.GONE);
+                        }
+
+                    }
+                });
+    }
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
 
         Intent intent = new Intent(this, Loading.class);
         startActivity(intent);
@@ -38,6 +100,28 @@ public class MainActivity extends AppCompatActivity {
         logoutButton = findViewById(R.id.logout);
         nickName = findViewById(R.id.nickname);
         profileImage = findViewById(R.id.profile);
+
+        GoogleSignInOptions googleSignInOptions = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString((R.string.default_web_client_id)))
+                .requestEmail()
+                .build();
+
+        googleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this,this)
+                .addApi(Auth.GOOGLE_SIGN_IN_API,googleSignInOptions)
+                .build();
+
+        auth = FirebaseAuth.getInstance(); //파이어베이스 인증 객체 초기화
+        btn_google = findViewById(R.id.btn_google);
+        btn_google.setOnClickListener(new View.OnClickListener() {  // 구글 로그인 버튼을 누르면 작동
+            @Override
+            public void onClick(View view) {
+                Intent intent = Auth.GoogleSignInApi.getSignInIntent(googleApiClient);
+                startActivityForResult(intent,REQ_SIGN_GOOGLE);
+            }
+        });
+
+
 
         Function2<OAuthToken, Throwable, Unit> callback = new Function2<OAuthToken, Throwable, Unit>() {
             @Override
@@ -99,15 +183,22 @@ public class MainActivity extends AppCompatActivity {
 
                     loginButton.setVisibility(View.GONE);
                     logoutButton.setVisibility(View.VISIBLE);
+                    btn_google.setVisibility(View.GONE);
                 } else {
                     nickName.setText(null);
                     profileImage.setImageBitmap(null);
                     loginButton.setVisibility((View.VISIBLE));
                     logoutButton.setVisibility(View.GONE);
+                    btn_google.setVisibility(View.VISIBLE);
 
                 }
                 return null;
             }
         });
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
     }
 }
