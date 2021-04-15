a7c9218645a79960d2d01cab650a7ae061f7a7ae
KoreaMango <mgo8434@naver.com>
2021-04-15 17:36:22 +0900

    ClubNameList

---
+++ app/src/main/java/com/example/swp_dongnae/ClubNameActivity.java
@@ -0,0 +1,14 @@
+package com.example.swp_dongnae;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ClubNameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_club_name);
+    }
+}
