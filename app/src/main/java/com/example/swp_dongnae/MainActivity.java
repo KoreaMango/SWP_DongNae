de3e31e7ac3f9f35cb641dc0ff252ab91a640d95
KoreaMango <mgo8434@naver.com>
2021-04-17 15:48:44 +0900

    ErrorModify_MGO

--- app/src/main/java/com/example/swp_dongnae/MainActivity.java
+++ app/src/main/java/com/example/swp_dongnae/MainActivity.java
@@ -16,7 +16,6 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
@@ -102,7 +101,7 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.O
     private RecyclerView recyclerView;
     private RecyclerView.Adapter adapter;
     private RecyclerView.LayoutManager layoutManager;
-    private ArrayList<CategoryActivity> arrayList;
+    private ArrayList<CategorySub> arrayList;
     private FirebaseDatabase database;
     private DatabaseReference databaseReference;
 
@@ -142,38 +141,6 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.O
                 startActivityForResult(intent,REQ_SIGN_GOOGLE);
             }
         });
-
-        recyclerView = findViewById(R.id.recyclerView); //리사이클뷰 아디 연결
-        recyclerView.setHasFixedSize(true); //리사이클러뷰 성능 강화
-        layoutManager = new LinearLayoutManager(this);
-        recyclerView.setLayoutManager(layoutManager);
-        arrayList = new ArrayList<>(); // 카테고리 액티비티 클래스의 객체를 담을 어레이 리스트
-
-        database = FirebaseDatabase.getInstance(); //파이어베이스 데이터 베이스 연동
-        databaseReference = database.getReference("CategoryActivity"); //db 테이블 연결
-        databaseReference.addListenerForSingleValueEvent(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                //파이어베이스 데이터베이스의 데이터를 받아오는곳
-                arrayList.clear(); //기존 배열리스트 초기화
-                for (DataSnapshot snapshot : dataSnapshot.getChildren()){  //반복문으로 데이터 리스트를 추출
-                    CategoryActivity categoryActivity = snapshot.getValue(CategoryActivity.class); //만들어둔 카테고리 액티비티 객체에 데이터를 담는다
-                    arrayList.add(categoryActivity);
-
-                }
-                adapter.notifyDataSetChanged();//리스트 저장 및 새로고침
-
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError databaseError) {
-                Log.e("MainActivity", String.valueOf(databaseError.toException()));   //에러문 출력
-            }
-        });
-        adapter =new CustomAdapter(arrayList,this);
-        recyclerView.setAdapter(adapter); //리사이클러뷰에 어댑터 연결
-
-
 
 
         Function2<OAuthToken, Throwable, Unit> callback = new Function2<OAuthToken, Throwable, Unit>() {
