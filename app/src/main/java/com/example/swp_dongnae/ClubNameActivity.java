742c48bbe16343ae3e03bd901cea04e1febed50d
bdh36 <bdh3620@naver.com>
2021-04-17 14:36:31 +0900

    error(second)_bdh

--- app/src/main/java/com/example/swp_dongnae/ClubNameActivity.java
+++ app/src/main/java/com/example/swp_dongnae/ClubNameActivity.java
@@ -10,5 +10,5 @@ public class ClubNameActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_club_name);
-    }
+    }T
 }
